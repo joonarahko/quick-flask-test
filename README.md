a6857a826157d3eb805adc11fb04be310f594fe0
joonarahko <joonarahko@gmail.com>
2019-02-15 21:07:27 -0800

    Source

---
+++ README.md
@@ -0,0 +1,7 @@
+# Steps to get running
+
+1. ```pip install Flask```
+
+2. ```python server.py```
+
+3. ```Open localhost:5000 on your browser```
---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, send_from_directory
+app = Flask(__name__, static_url_path='')
+
+
+PING_COUNT = 1
+
+@app.route('/')
+def index():
+    return send_from_directory('', 'index.html')
+
+
+@app.route('/api/todos', methods=['GET'])
+def todosController():
+    todos = [
+        {
+            'task': 'Clean room',
+            'done': False,
+        },
+        {
+            'task': 'Cook food',
+            'done': False,
+        }
+    ]
+
+    res = jsonify(todos)
+    res.status_code = 200
+
+    return res
+
+
+@app.route('/api/ping', methods=['POST'])
+def pingController():
+    global PING_COUNT
+    PING_COUNT += 1
+    print 'Button clicked {0} times'.format(PING_COUNT)
+    resp = jsonify(success=True)
+    return resp
+
+
+if __name__ == '__main__':
+    app.run()
